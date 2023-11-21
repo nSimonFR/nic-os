f77163d786150b9024a77ee983fcf02ba1beecaf
Nicolas Simon <nsimon@pm.me>
2023-11-21 22:57:46 +0100

    Add user authorized keys

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,7 +16,7 @@
         efiSysMountPoint = "/boot";
       };
       grub = {
-        devices = [ "nodev" ];
+        devices = ["nodev"];
         efiSupport = true;
         enable = true;
         extraEntries = ''
@@ -31,7 +31,7 @@
         '';
       };
     };
-    supportedFilesystems = [ "ntfs" ];
+    supportedFilesystems = ["ntfs"];
   };
 
   networking = {
@@ -140,14 +140,16 @@
   };
 
   programs.zsh.enable = true;
-
   users.defaultUserShell = pkgs.zsh;
+
   users.users.${username}= {
-    isNormalUser = true;
     home = "/home/${username}";
+    isNormalUser = true;
+    uid = 1000;
     extraGroups = [
       "wheel" # Enable ‘sudo’ for the user.
       "audio" # Enables pulseaudio management
     ];
+    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCfTlFbZh0ufzEysBxzaEhLU7A4J/n+c3ObaBr+nJPoovoBh9q4hB9KYwkr7y1wkkZgA6/aZqJu4HH2SCARGabyPJW2h2QY+IXs/pI7TV0eFaCP8SZjHWtz5rBm92pVSzXd/6/YoO+Ugn9EsuPYgnuGYlFaQ1BQrqCpJ7d+c9ZNU4mEKPNM5Ly/yo2V5Ox5nBfQg7jq9YIP0UFFwRe28Pi5OGCn0Wl+1aTOtd9sB06pXB4/CxCGRKZJLGe6QVMTFZJLObitjYpEX3zZ4Cj2MEiHVf6eubH0kTo6RSxYBZJBB2mmgBoDr9uae95LTXUBYoMPFb0dNYxzwe6HDZnqkBvlfsO6CHHAJYxqkRhxHgCy2gItJXpZ4HAPGezcnvBinTfuyf18Crb9wxiH5VaCaNaLhp66881KdLoMzNUTWU9L0ZRMzmabj0XgjpRLEqnTdvqq6H+NwYF1Avew07zwb8iZtbCIb2dxu653RxM8DwxmUnfmAAuxvxOoFpgYjDsDkahDEOynTkYWASbOoha66H5tU0mrAdeyooieHlFqAz/vjo5X/eIerWVrKEy0MdLx4Yu15ObTlWscU3qQyUmVlnH0SDg7ulH+4uNsXFE7jGHwg03MpYYAExTbPMpKlhJdaQI2Jzp3CcSqIG+1ODuPK8VcshTAtP0IrZ+ykFflB4EIcw=="];
   };
 }
