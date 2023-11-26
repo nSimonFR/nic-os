2aab281632b75ccaf0c01b5f8c40a200bd317c34
Nicolas Simon <nsimon@pm.me>
2023-11-26 13:03:24 +0100

    Refactor folder and remove programs.nix

--- home/default.nix
+++ home/default.nix
@@ -1,15 +1,80 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./vscode
-    ./programs.nix
+    ./zsh
   ];
 
-  home.stateVersion = "23.05";
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowUnfreePredicate = _: true;
     };
   };
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "23.05";
+  xdg.enable = true;
+
+  home.packages = with pkgs; [
+    awscli
+    bash
+    btop
+    coreutils-full
+    curl
+    ctop
+    direnv
+    docker
+    ed
+    gh
+    git
+    git-interactive-rebase-tool
+    git-extras
+    git-lfs
+    gnupg
+    gource
+    gzip
+    terraform
+    jq
+    k9s
+    kubeseal
+    less
+    nano
+    nmap
+    openssh
+    rclone
+    ripgrep
+    rsync
+    sops
+    time
+    tmux
+    tree
+    unzip
+    vim
+    watchman
+    wget
+    youtube-dl
+    yq
+    zsh
+
+    _1password
+    _1password-gui
+    spotify
+    slack
+
+    #inputs.nix-gaming.packages.${pkgs.system}.star-citizen
+
+    (pkgs.discord.override {
+      withOpenASAR = true;
+      # withVencord = true;
+    })
+    (pkgs.writeShellScriptBin "discord-fixed" ''
+      exec ${pkgs.discord}/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland
+    '')
+  ];
+
+  xdg.configFile."git/config".source = ./dotfiles/gitconfig;
+  xdg.configFile."git/ignore".source = ./dotfiles/gitignore;
+  xdg.configFile."tmux/tmux.conf".source = ./dotfiles/tmux.conf;
+  home.file.".vimrc".source = ./dotfiles/vim;
 }
---
+++ home/zsh/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    autocd = true;
+
+    history = {
+      ignoreDups = true;
+      save = 1000000;
+      size = 1000000;
+    };
+      
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "zsh-users/zsh-history-substring-search"; }
+        { name = "agkozak/zsh-z"; }
+        { name = "spaceship-prompt/spaceship-prompt"; }
+      ];
+    };
+
+    plugins = [
+      # Will load the whole folder and source nsimon.plugin.zsh:
+      { name = "nsimon"; src = ../dotfiles/zsh; }
+    ];
+  };
+}
