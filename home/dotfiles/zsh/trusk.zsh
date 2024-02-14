dab79ad96aad0468004fedad6ad5227feeb122db
Nicolas Simon <nsimon@pm.me>
2024-02-14 19:44:57 +0100

    Update trusk proxy alias and add encrypt/decrypt functions

--- home/dotfiles/zsh/trusk.zsh
+++ home/dotfiles/zsh/trusk.zsh
@@ -32,12 +32,32 @@ alias dc-mongo='dc exec mongo-db mongo trusk'
 alias dc-redis='dc exec redis-server redis-cli'
 alias dc-pgres='dc exec postgres-db psql -U postgres'
 
-alias k-seal="kubeseal -o yaml --controller-namespace sealed-secrets-system --controller-name sealed-secrets-sealed-secrets-operator"
-
 alias proxy-up="export http_proxy=localhost:8888 https_proxy=localhost:8888"
-alias proxy-prod="gcloud beta compute ssh trusk-production-gke-bastion --tunnel-through-iap --project trusk-production-kkypwi --zone europe-west1-c -- -L8888:127.0.0.1:8888"
-alias proxy-staging="gcloud beta compute ssh trusk-staging-gke-bastion --tunnel-through-iap --project trusk-staging-3rpyod --zone europe-west1-c -- -L8888:127.0.0.1:8888 -o ServerAliveInterval=60"
-alias proxy-db="gcloud beta compute ssh trusk-production-gke-bastion --tunnel-through-iap --project trusk-production-kkypwi --zone europe-west1-c"
 
-alias trusk-staging-rabbit="gcloud beta compute ssh bastion-lzrn --project trusk-playground --zone europe-west1-b -- -L15672:rabbitmq-cluster-staging-node-0:15672"
-alias trusk-staging-postgres="gcloud beta compute ssh bastion-lzrn --project trusk-playground --zone europe-west1-b -- -L5432:10.104.48.13:5432"
+alias proxy-prod="gcloud beta compute ssh trusk-production-gke-bastion --tunnel-through-iap --project trusk-production-kkypwi --zone europe-west1-c -- -fNT -M -S /tmp/trusk-production-gke-bastion.socket -L8888:127.0.0.1:8888 && proxy-up"
+alias proxy-prod-postgres="gcloud beta compute ssh trusk-production-gke-bastion --tunnel-through-iap --project trusk-production-kkypwi --zone europe-west1-c -- -fNT -M -S /tmp/trusk-production-gke-bastion.socket && proxy-up"
+alias proxy-prod-down="unset http_proxy https_proxy && ssh -S /tmp/trusk-production-gke-bastion.socket -O exit trusk-production-gke-bastion -q"
+
+alias proxy-staging="gcloud beta compute ssh trusk-staging-gke-bastion --tunnel-through-iap --project trusk-staging-3rpyod --zone europe-west1-c -- -fNT -M -S /tmp/trusk-staging-gke-bastion.socket -L8888:127.0.0.1:8888 -o ServerAliveInterval=60 && proxy-up"
+alias proxy-staging-down="unset http_proxy https_proxy && ssh -S /tmp/trusk-staging-gke-bastion.socket -O exit trusk-staging-gke-bastion -q"
+
+alias trusk-staging-rabbit="gcloud beta compute ssh bastion-lzrn --project trusk-playground --zone europe-west1-b -- -fNT -M -S /tmp/bastion-lzrn.socket -L15672:rabbitmq-cluster-staging-node-0:15672 && proxy-up"
+alias trusk-staging-postgres="gcloud beta compute ssh bastion-lzrn --project trusk-playground --zone europe-west1-b -- -fNT -M -S /tmp/bastion-lzrn.socket -L5432:10.104.48.13:5432 && proxy-up"
+alias proxy-tools-down="unset http_proxy https_proxy && ssh -S /tmp/bastion-lzrn.socket -O exit bastion-lzrn -q"
+
+function decrypt() {
+  eval "proxy-$1"
+  PATH_TO_KEY=~/MyDocuments/TRUSK/backup-sealed-secrets-$1.key
+  FILEPATH=`dirname $2`/DECRYPTED-`basename $2`
+  kubeseal --recovery-unseal -o yaml --recovery-private-key $PATH_TO_KEY < $2 > $FILEPATH
+  eval "proxy-$1-down"
+}
+
+function encrypt() {
+  eval "proxy-$1"
+  FILEPATH=`dirname $2`/DECRYPTED-`basename $2`
+  kubeseal -o yaml --controller-namespace sealed-secrets-system --controller-name sealed-secrets-sealed-secrets-operator < $FILEPATH > $2
+  rm $FILEPATH
+  eval "proxy-$1-down"
+}
+
