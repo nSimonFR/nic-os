22319ef49393a8a67a776372424118419285bb73
Nicolas Simon <nsimon@pm.me>
2024-02-19 16:28:59 +0100

    Add more macos CustomUserPreferences

--- macos/components/system.nix
+++ macos/components/system.nix
@@ -29,13 +29,53 @@
     };
 
     CustomUserPreferences = {
-      # 3 finger dragging
-      "com.apple.AppleMultitouchTrackpad".DragLock = false;
-      "com.apple.AppleMultitouchTrackpad".Dragging = false;
-      "com.apple.AppleMultitouchTrackpad".TrackpadThreeFingerDrag = true;
+      "com.apple.AppleMultitouchTrackpad" = {
+        DragLock = false;
+        Dragging = false;
+        TrackpadThreeFingerDrag = true;
+      };
 
-      # Finder's default location upon open
-      "com.apple.finder".NewWindowTargetPath = "file:///tmp";
+      "com.apple.finder" = {
+        NewWindowTargetPath = "file:///tmp";
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = true;
+        ShowMountedServersOnDesktop = true;
+        ShowRemovableMediaOnDesktop = true;
+        _FXSortFoldersFirst = true;
+        # When performing a search, search the current folder by default
+        FXDefaultSearchScope = "SCcf";
+      };
+
+      "com.apple.screensaver" = {
+        # Require password immediately after sleep or screen saver begins
+        askForPassword = 1;
+        askForPasswordDelay = 0;
+      };
+
+      "com.apple.AdLib" = {
+        allowApplePersonalizedAdvertising = false;
+      };
+
+      "com.apple.print.PrintingPrefs" = {
+        # Automatically quit printer app once the print jobs complete
+        "Quit When Finished" = true;
+      };
+
+      "com.apple.SoftwareUpdate" = {
+        AutomaticCheckEnabled = true;
+        # Check for software updates daily, not just once per week
+        ScheduleFrequency = 1;
+        # Download newly available updates in background
+        AutomaticDownload = 1;
+        # Install System data files & security updates
+        CriticalUpdateInstall = 1;
+      };
+
+      "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+      # Prevent Photos from opening automatically when devices are plugged in:
+      "com.apple.ImageCapture".disableHotPlug = true;
+      # Turn on app auto-update
+      "com.apple.commerce".AutoUpdate = true;
     };
 
     NSGlobalDomain = {
