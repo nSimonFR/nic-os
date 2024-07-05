745c0b610f68507f02e13001b6c8feaec5561e63
Nicolas Simon <nsimon@pm.me>
2024-07-05 11:41:11 +0200

    Yabai: use new version

--- macos/components/yabai.nix
+++ macos/components/yabai.nix
@@ -2,10 +2,10 @@
 {
   enable = true;
   package = (pkgs.yabai.overrideAttrs (o: rec {
-    version = "7.1.0";
+    version = "7.1.1";
     src = builtins.fetchTarball {
       url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "sha256:0gjlxgb2rfxryvg49nwwv01vh8d8pmni9v1873f0wv635skj9jl3";
+      sha256 = "sha256:0l31zppaq0jhnra1z2ddpj5rw4sz9f04wyja3sizsaad5acd94aw";
     };
   }));
   enableScriptingAddition = true;
