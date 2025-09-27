e57e98f8946e62253f775e3d7530c868f6340105
Nicolas Simon <nsimon@pm.me>
2025-09-27 11:24:03 +0200

    Update nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,10 +1,18 @@
-{ config, lib, pkgs, ... }:
-
-{
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+{ config, lib, pkgs, inputs, username, ... }:
+let
+  hyprlandSession =
+    (pkgs.writeTextDir "share/wayland-sessions/hyprland.desktop" ''
+      [Desktop Entry]
+      Name=Hyprland
+      Description=A dynamic tiling Wayland compositor
+      Exec=${pkgs.hyprland}/bin/Hyprland
+      Type=Application
+    '').overrideAttrs (old: { passthru.providedSessions = [ "hyprland" ]; });
+in {
+  imports = [
+    inputs.nix-gaming.nixosModules.platformOptimizations
+    ./hardware-configuration.nix
+  ];
 
   nixpkgs.config.allowUnfree = true;
 
@@ -20,10 +28,12 @@
       "nvidia.NVreg_EnableGpuSuspend=1"
     ];
 
-    kernel.sysctl = {
-      "vm.max_map_count" = 16777216;
-      "fs.file-max" = 524288;
-    };
+    # kernel.sysctl = {
+    #   "vm.max_map_count" = 16777216;
+    #   "fs.file-max" = 524288;
+    # };
+
+    kernelModules = [ "ff_memless" ];
 
     supportedFilesystems = [ "ntfs3" ];
   };
@@ -45,8 +55,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  # TODO: use username
-  users.users.nsimon = {
+  users.users.${username} = {
     isNormalUser = true;
     extraGroups = [
       "wheel" # Enables 'sudo'
@@ -57,8 +66,8 @@
       "systemd-journal"
       "docker"
       "input"
-    ]; 
-    home = "/home/nsimon";
+    ];
+    home = "/home/${username}";
 
     openssh.authorizedKeys.keys = [
       # TODO: fetch from github
@@ -67,11 +76,17 @@
   };
 
   environment.systemPackages = with pkgs; [
-    vim
-    wget
     cage
-    ntfs3g
     greetd.regreet
+    ntfs3g
+    openrgb-with-all-plugins
+    usbutils
+    vim
+    vulkan-tools
+    vulkan-loader
+    wayland
+    wayland-protocols
+    wget
   ];
 
   programs.gnupg.agent = {
@@ -87,6 +102,7 @@
   security.polkit.enable = true;
 
   hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
 
   hardware.nvidia = {
     open = true;
@@ -101,38 +117,54 @@
     };
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.cage}/bin/cage -s ${pkgs.greetd.regreet}/bin/regreet";
+        command =
+          "${pkgs.cage}/bin/cage -m last -s ${pkgs.greetd.regreet}/bin/regreet";
         user = "greeter";
       };
     };
   };
 
-  programs.regreet.enable = true;
+  programs.regreet = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    settings = {
+      background = {
+        fit = "Cover";
+        path = "/home/${username}/wallpaper.png";
+      };
+      GTK = { application_prefer_dark_theme = true; };
+    };
+  };
 
   users.users.greeter = {
     isSystemUser = true;
     group = "greeter";
   };
 
+  services.displayManager.sessionPackages = [ hyprlandSession ];
+
   environment.etc = {
     "1password/custom_allowed_browsers" = {
+      # FIXME
       text = ''
-        .zen-wrapped
-        .zen-twilight-wrapped
+        zen-twilight
       '';
       mode = "0755";
     };
   };
 
-  security.pam.services.hyprlock = {
-    allowNullPassword = false;
-  };
+  security.pam.services.hyprlock = { allowNullPassword = false; };
 
   services.pipewire = {
     enable = true;
@@ -144,18 +176,39 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     extest.enable = true;
+    gamescopeSession.enable = true;
     localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
+    platformOptimizations.enable = true;
   };
+
+  programs.gamemode.enable = true;
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
   hardware.steam-hardware.enable = true;
 
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp;
+    rulesProvider = pkgs.ananicy-cpp;
+    extraRules = [{
+      "name" = "gamescope";
+      "nice" = -20;
+    }];
+  };
+
   fileSystems."/mnt/games" = {
-    device = "/dev/nvme1n1p2";
+    device = "/dev/disk/by-label/Games\\x20SSD";
     fsType = "ntfs3";
     options = [
       "uid=1000"
       "gid=100"
       "umask=0000"
+      "force"
       "nofail"
       "x-systemd.automount"
       "noauto"
@@ -164,16 +217,17 @@
 
   swapDevices = [{
     device = "/var/lib/swapfile";
-    size = 8 * 1024;  # 8 GB Swap
+    size = 8 * 1024; # 8 GB Swap
   }];
 
   zramSwap = {
     enable = true;
-    memoryMax = 16 * 1024 * 1024 * 1024;  # 16 GB ZRAM
+    memoryMax = 16 * 1024 * 1024 * 1024; # 16 GB ZRAM
   };
 
   services.udev = {
     extraRules = ''
+      SUBSYSTEM=="input", GROUP="input", MODE="0660"
       KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0666", TAG+="uaccess"
     '';
 
@@ -182,15 +236,32 @@
 
   #system.activationScripts.applyWineDualsenseFix.text = builtins.readFile ./scripts/wine-dualsense-fix.sh;
 
+  networking.firewall = rec {
+    allowedTCPPortRanges = [{
+      from = 1714;
+      to = 1764;
+    }];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+
+  virtualisation.docker = { enable = true; };
+
+  services.hardware.openrgb.enable = true;
+
   services.dbus.enable = true;
   hardware.uinput.enable = true;
-  hardware.bluetooth.enable = true;
 
-  system.stateVersion = "25.05"; # DO NOT UPDATE UNLESS YOU KNOW WHAT YOU'RE DOING
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  system.stateVersion =
+    "25.05"; # DO NOT UPDATE UNLESS YOU KNOW WHAT YOU'RE DOING
 
   nix.settings = {
-    substituters = ["https://nix-gaming.cachix.org"];
-    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+    substituters = [ "https://nix-gaming.cachix.org" ];
+    trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
     experimental-features = [ "nix-command" "flakes" ];
   };
 }
