c5337825c89badd2fce09917ba821a5d81eb2119
Nicolas Simon <nsimon@pm.me>
2026-02-23 10:58:15 +0100

    fix(ci): make local commitlint hook self-contained

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
       'always',
       ['feat', 'fix', 'docs', 'refactor', 'chore', 'ci', 'test', 'perf', 'build', 'revert'],
     ],
+    'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 100],
   },
